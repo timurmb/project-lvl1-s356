972fd3feb1cf0ee32641a23d85ff6a65cfe38123
timur meirbekov <timur.meirbekov@gmail.com>
2018-10-02 23:44:57 +0600

    step3 many changes)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-export const askName = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hi, ${userName}!`);
-    return;
-}
+const askName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi, ${userName}!`);
+};
+
+export default askName;
