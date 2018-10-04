93623022965e8f0706b0b7565f752365ef9484a2
timur meirbekov <timur.meirbekov@gmail.com>
2018-10-04 20:46:52 +0600

    step5

--- src/index.js
+++ src/index.js
@@ -12,12 +12,51 @@ const askName = () => {
   return userName;
 };
 
-// return random number 1-100
-const getRandom = () => Math.floor(Math.random() * 100 + 1);
+// return random number from 1 to 'max'
+const getRandom = (max = 100) => {
+  return Math.floor(Math.random() * max + 1);
+};
 
 const isEven = (number) => {
   if (number % 2 === 0) return true;
   return false;
+};
+
+const getOperation = () => {
+  const selector = Math.floor(Math.random() * 3 + 1);
+  let operation;
+  let operationAsSymbol;
+  switch (selector) {
+    case 1:
+      operation = (num1, num2) => num1 + num2;
+      operationAsSymbol = '+';
+      break;
+    case 2:
+      operation = (num1, num2) => num1 - num2;
+      operationAsSymbol = '-';
+      break;
+    case 3:
+      operation = (num1, num2) => num1 * num2;
+      operationAsSymbol = '*';
+      break;
+    default:
+      break;
+  }
+  return { oper: operation, symb: operationAsSymbol };
+};
+
+const check = (answer, correctAnswer, userName) => {
+  if (answer !== correctAnswer) {
+    console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+    console.log(`Let's try again, ${userName}!`);
+    return false;
+  }
+  console.log('Correct!');
+  return true;
+};
+
+const congrat = (userName) => {
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export const gameEven = () => {
@@ -30,14 +69,27 @@ export const gameEven = () => {
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
     const correctAnswer = isEven(question) ? 'yes' : 'no';
-    if (answer !== correctAnswer) {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-    console.log('Correct!');
+    if (check(answer, correctAnswer, userName) === false) return;
   }
-  console.log(`Congratulations, ${userName}!`);
+  congrat(userName);
+};
+
+export const gameCalc = () => {
+  greeting();
+  console.log('What is the result of the expression?');
+  const userName = askName();
+
+  for (let round = 1; round <= numberOfRounds; round += 1) {
+    const operation = getOperation();
+    const num1 = getRandom(10);
+    const num2 = getRandom(10);
+    const question = `${num1} ${operation.symb} ${num2}`;
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = operation.oper(num1, num2).toString();
+    if (check(answer, correctAnswer, userName) === false) return;
+  }
+  congrat(userName);
 };
 
 export default askName;
